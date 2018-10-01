c2346ecdc7e74d2276b3df628c5a49985bfb60cb
Arnaud Ehresmann <arnaud.ehresmann@gmail.com>
2018-10-01 18:26:07 +0200

    cluster styling

--- App.js
+++ App.js
@@ -6,21 +6,10 @@ import breweries from './map/features.json'
 import Bubble from './components/bubble'
 
 Mapbox.setAccessToken(config.get('accessToken'));
-
-const VECTOR_SOURCE_URL =
-  'mapbox://arnaudehresmann.cjmag27gf2ywn32s5vwzc8uvj-7ic0r';
+const mapUrl = config.get('mapUrl');
 
   const layerStyles = Mapbox.StyleSheet.create({
-    symbol:{
-        textField:'title'
-    },
-    singlePoint: {
-      // circleColor: 'green',
-      // circleOpacity: 0.84,
-      // circleStrokeWidth: 2,
-      // circleStrokeColor: 'white',
-      // circleRadius: 5,
-      // circlePitchAlignment: 'map',
+    singleBrewery: {
       textField: '{title}',
       textSize: 12,
       textPitchAlignment: 'map',
@@ -30,27 +19,14 @@ const VECTOR_SOURCE_URL =
       textOptional: true
     },
   
-    clusteredPoints: {
+    clusteredBreweries: {
       circlePitchAlignment: 'map',
-      circleColor: Mapbox.StyleSheet.source(
-        [
-          [25, 'yellow'],
-          [50, 'red'],
-          [75, 'blue'],
-          [100, 'orange'],
-          [300, 'pink'],
-          [750, 'white'],
-        ],
-        'point_count',
-        Mapbox.InterpolationMode.Exponential,
-      ),
-  
+      circleColor: 'orange',
       circleRadius: Mapbox.StyleSheet.source(
         [[0, 15], [100, 20], [750, 30]],
         'point_count',
         Mapbox.InterpolationMode.Exponential,
       ),
-  
       circleOpacity: 0.84,
       circleStrokeWidth: 2,
       circleStrokeColor: 'white',
@@ -89,8 +65,6 @@ const VECTOR_SOURCE_URL =
     }
 
     onPress(event) {
-        const feature = event.nativeEvent.payload;
-        console.log('You pressed a layer here is your feature', feature); // eslint-disable-line
       const { geometry, properties } = feature;
   
       this.setState({
@@ -120,37 +94,36 @@ const VECTOR_SOURCE_URL =
       return (
         <View style={styles.container}>
           <Mapbox.MapView
-              styleURL={'mapbox://styles/arnaudehresmann/cjmolasbs01e42sljq5fz78sy'}
+              styleURL={mapUrl}
               zoomLevel={4.81}
               centerCoordinate={[3.315401, 47.077385]}
               style={styles.container}
               
               >
          <Mapbox.ShapeSource
-            id="earthquakes"
+            id="breweries"
             cluster
-            clusterRadius={15}
-            clusterMaxZoom={14}
+            clusterRadius={25}
+            clusterMaxZoom={12}
             shape={breweries}
             onPress={this.onPress}
             >
             <Mapbox.SymbolLayer
               id="pointCount"
               style={layerStyles.clusterCount}
-              belowLayerID="clusteredPoints"
             />
 
             <Mapbox.CircleLayer
-              id="clusteredPoints"
+              id="clusteredBreweries"
               belowLayerID="pointCount"
               filter={['has', 'point_count']}
-              style={layerStyles.clusteredPoints}
+              style={layerStyles.clusteredBreweries}
             />
 
             <Mapbox.SymbolLayer
-              id="singlePoint"
+              id="singleBrewery"
               filter={['!has', 'point_count']}
-              style={layerStyles.singlePoint}
+              style={layerStyles.singleBrewery}
               belowLayerID="pointCount"
             />
           </Mapbox.ShapeSource>      
