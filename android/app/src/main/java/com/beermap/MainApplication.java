844e83dac55d9d0d87e8bbe1a368bb8515daaf76
Arnaud Ehresmann <arnaud.ehresmann@gmail.com>
2018-10-24 14:57:54 +0200

    add firebase authentication

--- android/app/src/main/java/com/beermap/MainApplication.java
+++ android/app/src/main/java/com/beermap/MainApplication.java
@@ -3,6 +3,9 @@ package com.beermap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.links.RNFirebaseLinksPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNVersionNumberPackage(),
-          new RCTMGLPackage()
+          new RCTMGLPackage(),
+          new RNFirebaseLinksPackage(),
+          new RNFirebaseAuthPackage()
       );
     }
 
