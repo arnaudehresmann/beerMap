abacf2bad2b11c7d54afefcd7c87564e052b64ee
Arnaud Ehresmann <arnaud.ehresmann@gmail.com>
2018-11-12 16:47:45 +0100

    don't crash when offline

--- app/screens/Loading.js
+++ app/screens/Loading.js
@@ -1,28 +1,55 @@
 import React from 'react'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, StyleSheet, NetInfo, ConnectionType } from 'react-native'
 import firebase, { config } from 'react-native-firebase'
 import Config from '../utils/Config'
 import { CreateStore } from '../utils/BreweryStore'
 export default class Loading extends React.Component {
 
     async componentDidMount() {
-      try {
-        let response = await fetch(
-          Config.get("breweriesUrl")
-        );
-        let breweries = await response.json();
-        CreateStore(breweries);
-        
-      } catch (error) {
-        console.error(error);
+      let connectionInfo = await NetInfo.getConnectionInfo();
+
+      let breweries = undefined;
+
+      let isConnected = await NetInfo.isConnected.fetch();
+      if(isConnected) {
+        breweries = await this.getOnlineBreweries();
+      } else {
+        breweries = await this.getOfflineBreweries();
       }
+
+      CreateStore(breweries);
       
       firebase.auth().onAuthStateChanged(user => {          
           this.props.navigation.navigate(user ? 'Main' : 'Main');
         });
 
-        return await Promise.resolve(1);
-    }
+      return await Promise.resolve(1);
+  }
+
+  async getOnlineBreweries() {
+    let breweries = {
+        "type": "FeatureCollection",
+        "features":[]
+    };
+
+    try {
+      let response = await fetch(
+        Config.get("breweriesUrl")
+      );
+      breweries = await response.json();
+      
+    } catch (error) {
+      console.error(error);
+    }  
+    return breweries;
+  }
+
+  async getOfflineBreweries() {
+    return {
+        "type": "FeatureCollection",
+        "features":[]
+    };
+  }
 
   render() {
     return (
